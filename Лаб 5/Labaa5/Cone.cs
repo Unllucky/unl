79d924aae3559338ecbb7925d52fe62576017e25
Demchenko Artem <zabubon@outlook.com>
2020-11-29 04:19:40 +0100

    лаб 5

---
+++ Лаб 5/Labaa5/Cone.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labaa5
+{
+    class Cone
+    {
+        protected double BigRadius { get; set; }
+        protected double Hieght { get; set; }
+        protected double Generetix { get; set; }
+
+        public Cone(double rad, double hieght,double gen) => (BigRadius, Hieght,Generetix) = (rad, hieght,gen);
+        public virtual double GetVol()=>
+            (Math.Pow(BigRadius, 2) * Hieght) / 3;
+        public virtual double GetSquare() =>
+            Math.Pow(BigRadius, 2) * BigRadius*Generetix;
+        public virtual double GetBigRad() => BigRadius;
+        public virtual double GetHieght() => Hieght;
+        public virtual double Getgen() => Generetix;
+
+
+
+    }
+}
---
+++ Лаб 5/Labaa5/TringleMatrix.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labaa5
+{
+    class TringleMatrix : Matrix
+    {
+        public TringleMatrix(int m, int n) : base(m, n)
+        {
+            (M, N) = (m, n);
+            data = new double[m, n];
+        }
+
+        public override void Show()
+        {
+            for (int i = 0; i < M; i++)
+            {
+                for (int j = 0; j < N; j++)
+                    Console.Write(data[i, j] + " ");
+                Console.WriteLine();
+            }
+        }
+
+        public override void Fill()
+        {
+            Random r = new Random();
+            
+            for (int i = 0; i < M; i++)
+                for (int j = 0; j < N; j++)
+                {
+                    if(i<=j)
+                        data[i, j] = r.Next(0,10);
+                    else
+                    {
+                        data[i, j] = 0;
+                    }
+
+
+                }
+
+        }
+    }
+}
